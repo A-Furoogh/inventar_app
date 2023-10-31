1930daf9464474e4ca7f7e99b6459ed46e92401d
Furoogh <ahmad.furoogh@gmail.com>
2023-10-31 17:01:29 +0100

    changes

--- lib/pages/login_page/login_page.dart
+++ lib/pages/login_page/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:inventar_app/pages/home_page/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:inventar_app/blocs/auth_bloc/auth_bloc.dart';
+import 'package:inventar_app/repositories/auth_repository.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,83 +12,94 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
+  final AuthRepository _authRepository = AuthRepository();
+
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Row(
-        children: [
-          Icon(Icons.account_circle_outlined, color: Colors.white),
-          SizedBox(width: 10),
-          Text('Anmelden', style: TextStyle(fontSize: 20)),
-        ],
-      ), centerTitle: true),
-      body: Container(
-        color: Colors.grey[200],
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Anmelden', style: TextStyle(fontSize: 30)),
-            Padding(
-              padding: const EdgeInsets.all(40.0),
-              child: Column(
-                children: [
-                  const SizedBox(height: 20),
-                  Row(
+    return BlocProvider(
+      create: (context) => AuthBloc(_authRepository),
+      child: Scaffold(
+        appBar: AppBar(
+            title: const Row(
+              children: [
+                Icon(Icons.account_circle_outlined, color: Colors.white),
+                SizedBox(width: 10),
+                Text('Anmelden', style: TextStyle(fontSize: 20)),
+              ],
+            ),
+            centerTitle: true),
+        body: Form(
+          key: _formKey,
+          child: Container(
+            color: Colors.grey[200],
+            child: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Anmelden', style: TextStyle(fontSize: 30)),
+                Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Column(
                     children: [
-                      Icon(Icons.person_3_rounded, color: Colors.grey[600]),
-                      Expanded(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                              hintText: 'Benutzername',
-                              border: OutlineInputBorder(),
-                              fillColor: Colors.white70,
-                              filled: true),
-                          controller: _usernameController,
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Icon(Icons.person_3_rounded, color: Colors.grey[600]),
+                          Expanded(
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                  hintText: 'Benutzername',
+                                  border: OutlineInputBorder(),
+                                  fillColor: Colors.white70,
+                                  filled: true),
+                              controller: _usernameController,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Icon(Icons.key_rounded, color: Colors.grey[600]),
+                          Expanded(
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                  hintText: 'Passwort',
+                                  border: OutlineInputBorder(),
+                                  fillColor: Colors.white70,
+                                  filled: true),
+                              controller: _passwordController,
+                              obscureText: true,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          BlocProvider.of<AuthBloc>(context)
+                            .add(LoginEvent(_usernameController.text, _passwordController.text)
+                            // Hier muss auch die SharedPreferences gesetzt werden.
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(200, 50),
                         ),
-                      )
+                        child: const Text('anmelden',
+                            style: TextStyle(fontSize: 20)),
+                      ),
                     ],
                   ),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Icon(Icons.key_rounded, color: Colors.grey[600]),
-                      Expanded(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                              hintText: 'Passwort',
-                              border: OutlineInputBorder(),
-                              fillColor: Colors.white70,
-                              filled: true),
-                          controller: _passwordController,
-                          obscureText: true,
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const HomePage()),
-                        // Hier muss auch die SharedPreferences gesetzt werden.
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(200, 50),
-                    ),
-                    child:
-                        const Text('anmelden', style: TextStyle(fontSize: 20)),
-                  ),
-                ],
-              ),
-            )
-          ],
-        )),
+                )
+              ],
+            )),
+          ),
+        ),
       ),
     );
   }
